4e8fb038fc3f17500596fc2ac1a12fc5062cd034
Nikola Begedin <begedinnikola@gmail.com>
2016-11-10 20:05:57 +0100

    Mocked out stripe service for testng purpose. Made process more robust. Added basic acceptance test for stripe error (needs to be finalized)

--- tests/acceptance/project-donate-test.js
+++ tests/acceptance/project-donate-test.js
@@ -1,13 +1,43 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'code-corps-ember/tests/helpers/module-for-acceptance';
+import Ember from 'ember';
 import { authenticateSession } from 'code-corps-ember/tests/helpers/ember-simple-auth';
 import createOrganizationWithSluggedRoute from 'code-corps-ember/tests/helpers/mirage/create-organization-with-slugged-route';
-import Ember from 'ember';
 import projectDonatePage from '../pages/project/donate';
 
 const {
-  Test: { registerWaiter, unregisterWaiter }
+  RSVP,
+  Service
 } = Ember;
+
+// NOTE: Don't think these mocks can be moved, unless we can make them more generic than they are
+// As is, they mock specifically the `stripe` injection for the project.donate controller
+
+const stripeCardError = {
+  error: {
+    type: 'card_error',
+    code: 'invalid_expiry_year',
+    message: "Your card's expiration year is invalid.",
+    param: 'exp_year'
+  }
+};
+
+const stripeMockSuccess = {
+  card: {
+    createToken: () => RSVP.resolve({ id: 'tok_tst12345' })
+  }
+};
+
+const stripeMockFailure = {
+  card: {
+    createToken: () => RSVP.reject(stripeCardError)
+  }
+};
+
+function stubStripe(context, mock) {
+  let mockService = Service.create(mock);
+  context.application.__container__.lookup('controller:project.donate').set('stripe', mockService);
+}
 
 moduleForAcceptance('Acceptance | Project - Donate');
 
@@ -31,6 +61,8 @@ test('It requires authentication', function(assert) {
 test('Allows adding a card and donating (creating a subscription)', function(assert) {
   assert.expect(3);
 
+  stubStripe(this, stripeMockSuccess);
+
   let user = server.create('user');
   authenticateSession(this.application, { 'user_id': user.id });
 
@@ -51,23 +83,7 @@ test('Allows adding a card and donating (creating a subscription)', function(ass
   });
 
   andThen(() => {
-    // needs to be done in a separate "andThen", due to async credit card validation
-    console.log('button disabled', find('.credit-card-form button').prop('disabled'));
     projectDonatePage.creditCard.clickSubmit();
-  });
-
-  // ember doesn't wait for stripe service to create a token, so the whole process doesn't happen
-  // in order to circumvent the issue, we register a waiter which periodically check if subscription was
-  // set on the controller. It's ugly, but I have no better idea
-  // If we change how the controller behaves, for example, if we do not set the loading flag and instead
-  // just transition to a new route, this will basically hang.
-  function checkLoadingFlag() {
-    let isLoading = this.application.__container__.lookup('controller:project.donate').get('isLoading');
-    return isLoading === false;
-  }
-
-  andThen(() => {
-    registerWaiter(this, checkLoadingFlag);
   });
 
   andThen(() => {
@@ -78,12 +94,41 @@ test('Allows adding a card and donating (creating a subscription)', function(ass
     assert.equal(subscription.projectId, project.id, 'Project was set to current project.');
   });
 
-  andThen(() => {
-    unregisterWaiter(this, checkLoadingFlag);
-  });
-
   // TODO: Add assertions to check whatever needs to happen on success happens
 });
 
 // TODO: test handling validation errors error (showing on page)
-// TODO: test handling stripe errors
+
+test('Shows stripe errors when creating card token fails', function(assert) {
+  assert.expect(1);
+
+  stubStripe(this, stripeMockFailure);
+
+  let user = server.create('user');
+  authenticateSession(this.application, { 'user_id': user.id });
+
+  let organization = createOrganizationWithSluggedRoute();
+  let project = server.create('project', { organization });
+
+  projectDonatePage.visit({
+    amount: 10,
+    organization: organization.slug,
+    project: project.slug
+  });
+
+  andThen(() => {
+    projectDonatePage.creditCard.cardNumber('4242-4242-4242-4242');
+    projectDonatePage.creditCard.cardCVC('123');
+    projectDonatePage.creditCard.cardMonth('12');
+    projectDonatePage.creditCard.cardYear('2020');
+  });
+
+  andThen(() => {
+    projectDonatePage.creditCard.clickSubmit();
+  });
+
+  andThen(() => {
+    assert.notOk(server.schema.stripeSubscriptions.findBy({ amount: 1000 }), 'Subscription was not created.');
+    // TODO: Further test errors are displayed
+  });
+});
